9fe2b6a3de4a50caef6fcffcfe978cc3b2ea9047
andre_jeon <andre.jeon@balancehero.com>
2019-03-22 11:10:13 +0900

    Fix return message

--- main.py
+++ main.py
@@ -3,7 +3,10 @@ import os
 # import logging
 import time
 from slackclient import SlackClient
+from langdetect import detect
+from nltk.tokenize import word_tokenize
 
+EXCUSES = '답변이 불만족스러운 경우, 앨런에게 직접 연락 바랍니다.'
 
 def help_message():
     return '''
@@ -11,10 +14,30 @@ def help_message():
 '''
 
 
+def create_resp(cmd):
+    return '자연어를 우짠다.'
+
+
 def smart_resp(cmd):
-    if cmd.startswith('hey') is False:
+    if (cmd.startswith('allen') is False and
+            cmd.startswith('Allen') is False and
+            cmd.startswith('ALLEN') is False and
+            cmd.startswith('알랜') is False and
+            cmd.startswith('앨런') is False):
+        # 앨런을 부르는게 아닌 경우 무시
         return
-    return cmd[3:].lstrip()
+
+    # TODO: 여기가 핵이다.
+    resp = create_resp(cmd)
+
+    lang = detect(cmd[0:2])
+    if lang == 'ko':
+        msg = '*문의*\n> %s\n*답변*\n> %s\n\n:arrow_right: %s' % (cmd[3:].lstrip(), resp, EXCUSES)
+    else:
+        msg = '*문의*\n> %s\n*답변*\n> %s\n\n:arrow_right: %s' % (cmd[5:].lstrip(), resp, EXCUSES)
+        # msg = '문의\n> %s\n답변\n> %s\n\n%s' % (cmd[5:].lstrip(), resp, EXCUSES)
+        # msg = '문의: %s\n답변: %s\n\n%s' % (cmd[5:].lstrip(), resp, EXCUSES)
+    return msg
 
 
 def handle_command(slack_client, channel, cmd):
@@ -39,7 +62,7 @@ def parse_bot_commands(slack_events):
         If a bot command is found, this function returns a tuple of command and channel.
         If its not found, then this function returns None, None.
     """
-    print(slack_events)
+    # print(slack_events)
     for event in slack_events:
         if event["type"] == "message" and "subtype" not in event:
             return event['channel'], event['text']
@@ -73,7 +96,7 @@ def main():
                 slack_events = slack_client.rtm_read()
                 channel, cmd = parse_bot_commands(slack_events)
                 if cmd:
-                    print(channel, cmd)
+                    # print(channel, cmd)
                     handle_command(slack_client, channel, cmd)
             except:
                 pass
